0242d3702bef2bb7d4e7489228fa8162bbd80208
nv <navjotshrivastava46@gmail.com>
2022-12-21 01:51:07 +0530

    fc

---
+++ .devcontainer/codespace.config.php
@@ -0,0 +1,19 @@
+<?php
+
+$codespaceName = getenv('CODESPACE_NAME');
+$codespaceDomain = getenv('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN');
+
+$CONFIG = [
+    'mail_from_address' => 'no-reply',
+    'mail_smtpmode' => 'smtp',
+    'mail_sendmailmode' => 'smtp',
+    'mail_domain' => 'example.com',
+    'mail_smtphost' => 'localhost',
+    'mail_smtpport' => '1025',
+    'memcache.local' => '\OC\Memcache\APCu',
+];
+
+if(is_string($codespaceName) && !empty($codespaceName) && is_string($codespaceDomain) && !empty($codespaceDomain)) {
+    $CONFIG['overwritehost'] = $codespaceName . '-80.' . $codespaceDomain;
+    $CONFIG['overwriteprotocol'] = 'https';
+}
